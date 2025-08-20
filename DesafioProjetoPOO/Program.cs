39faa9e6a0bac367344bf0185c0ddb518ea55aad
ANDERSON DOS SANTOS BARBOSA <51709894+AndersonSBarbosa@users.noreply.github.com>
2025-08-20 02:09:36 -0300

    Add project files.

---
+++ DesafioProjetoPOO/Program.cs
@@ -0,0 +1,23 @@
+﻿using DesafioProjetoPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando a classe Nokia
+        Nokia nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "111111111111111", memoria: 32);
+        Console.WriteLine("Testando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Testando a classe Iphone
+        Iphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 14", imei: "222222222222222", memoria: 128);
+        Console.WriteLine("Testando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
